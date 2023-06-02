0f2d8560ff2f120947ed773f7451e0617f9d6127
arx827 <arx827@gmail.com>
2023-06-02 22:55:17 +0800

    [feature] 『favicon』更改路徑

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,28 +2,35 @@
 import { defineConfig } from "vitepress";
 import { head, nav, sidebar } from './configs'
 
+// 主題 配置
+const themeConfig = {
+  logo: '/apple-touch-icon.png',
+  siteTitle: 'Java 程式設計',
+  
+  nav,
+  sidebar,
+
+  // socialLinks: [
+  //   { icon: 'github', link: 'https://github.com/gumingWu/vitepress-fun' }
+  // ],
+  footer: {
+    copyright: 'MIT Lincensed | Copyright © 2023-present Eason',
+  }
+}
+
+// markdown 配置
+const markdown = {
+  lineNumbers: true
+}
+
 export default defineConfig({
   base: '/vitepress-java/',   // 站點將部署在這個 base URL路徑
+  lang: 'zh-TW',
   title: 'Java 程式設計',
   description: '閱讀筆記',
-  themeConfig: {
-    logo: '/apple-touch-icon.png',
-    siteTitle: 'Java 程式設計',
-    
-    head,
-    nav,
-    sidebar,
-
-    // socialLinks: [
-    //   { icon: 'github', link: 'https://github.com/gumingWu/vitepress-fun' }
-    // ],
-    markdown: {
-      lineNumbers: true,    // 行號
-    },
-    footer: {
-      copyright: 'MIT Lincensed | Copyright © 2023-present Eason',
-    }
-  },
+  head,
+  themeConfig,
+  markdown,
   vite: {
     server: {
       port: 8501
--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -1,12 +1,13 @@
 import type { HeadConfig } from "vitepress";
 
+
 export const head: HeadConfig[] = [
     // ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/vitepress-java/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/vitepress-java/favicons/favicon-16x16.png"}],
     // ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
     // ['link', { rel: "mask-icon", href: "/assets/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
-    ['link', { rel: "shortcut icon", href: "/favicons/favicon.ico"}],
+    ['link', { rel: "shortcut icon", href: "/vitepress-java/favicons/favicon.ico"}],
     // ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
     // ['meta', { name: "msapplication-config", content: "/assets/favicons/browserconfig.xml"}],
     // ['meta', { name: "theme-color", content: "#ffffff"}],
